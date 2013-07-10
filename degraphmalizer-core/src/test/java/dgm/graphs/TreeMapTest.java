778c4fc1d1dbc3149350096c5e7970ba75c02e65
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2013-07-10 18:17:02 +0200

    simplified

--- degraphmalizer-core/src/test/java/dgm/graphs/TreeMapTest.java
+++ degraphmalizer-core/src/test/java/dgm/graphs/TreeMapTest.java
@@ -4,26 +4,31 @@
  */
 package dgm.graphs;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.base.Function;
-import com.google.common.base.Optional;
-import com.tinkerpop.blueprints.Direction;
-import com.tinkerpop.blueprints.Edge;
-import com.tinkerpop.blueprints.Vertex;
 import dgm.EdgeID;
 import dgm.GraphUtilities;
 import dgm.ID;
 import dgm.Subgraph;
 import dgm.trees.Pair;
 import dgm.trees.Tree;
+import dgm.trees.TreeEntry;
 import dgm.trees.Trees;
+
+import java.util.Iterator;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.google.common.collect.Iterables;
+import com.tinkerpop.blueprints.Direction;
+import com.tinkerpop.blueprints.Edge;
+import com.tinkerpop.blueprints.Vertex;
 
 import static org.fest.assertions.Assertions.assertThat;
 
@@ -32,28 +37,24 @@ import static org.fest.assertions.Assertions.assertThat;
  * Date: 13/05/2013
  */
 @Test
-public class TreeMapTest
-{
+public class TreeMapTest {
     LocalGraph lg;
     RandomizedGraphBuilder gb;
     ObjectMapper om = new ObjectMapper();
     ExecutorService executor = Executors.newCachedThreadPool();
 
     @BeforeMethod
-    public void clearGraph()
-    {
+    public void clearGraph() {
         gb = new RandomizedGraphBuilder(0);
         lg = LocalGraph.localNode();
     }
 
     @AfterMethod
-    public void shutdownGraph()
-    {
+    public void shutdownGraph() {
         lg.G.shutdown();
     }
 
-    public void testOptionalTree() throws ExecutionException, InterruptedException
-    {
+    public void testOptionalTree() throws ExecutionException, InterruptedException {
         final EdgeID e1 = gb.edge("   (a,b,c,1) -- label --> (a,b,d,1)   ");
         final EdgeID e2 = gb.edge("   (a,b,d,1) -- label --> (a,b,e,1)   ");
         final EdgeID e3 = gb.edge("   (a,b,d,1) -- label --> (a,b,f,1)   ");
@@ -87,12 +88,11 @@ public class TreeMapTest
 
         Vertex rootVertex = GraphUtilities.findVertex(om, lg.G, root);
         Tree<Pair<Edge, Vertex>> tree = GraphUtilities.childrenFrom(rootVertex, Direction.OUT);
-        int treeSize = countIterator(Trees.bfsWalk(tree));
-        assertThat(treeSize).isEqualTo(countIterator(lg.G.getVertices()));
+        int treeSize = Iterables.size(Trees.bfsWalk(tree));
+        assertThat(treeSize).isEqualTo(Iterables.size(lg.G.getVertices()));
 
         System.err.println("Root " + root);
-        for (Edge e : lg.G.getEdges())
-        {
+        for (Edge e : lg.G.getEdges()) {
             final EdgeID edgeId = GraphUtilities.getEdgeID(om, e);
             if (edgeId == null)
                 System.err.println(e.toString());
@@ -101,13 +101,11 @@ public class TreeMapTest
         }
 
         final ID missingID = e6.head();
-        final Counter visited = new Counter();
-        final Function<Pair<Edge, Vertex>, Optional<Node>> pairToNode = new Function<Pair<Edge, Vertex>, Optional<Node>>()
-        {
+        final int[] visited = new int[1];
+        final Function<Pair<Edge, Vertex>, Optional<Node>> pairToNode = new Function<Pair<Edge, Vertex>, Optional<Node>>() {
             @Override
-            public Optional<Node> apply(Pair<Edge, Vertex> input)
-            {
-                visited.increment();
+            public Optional<Node> apply(Pair<Edge, Vertex> input) {
+                visited[0]++;
                 if (GraphUtilities.getID(om, input.b).equals(missingID)) {
                     return Optional.absent();
                 }
@@ -116,11 +114,11 @@ public class TreeMapTest
         };
 
         Tree<Optional<Node>> nodeTree = Trees.pmap(executor, pairToNode, tree);
-        assertThat(visited.count()).isEqualTo(1);
+        assertThat(visited[0]).isEqualTo(1);
 
-        Iterable<Optional<Node>> nodeIterator = Trees.bfsWalk(nodeTree);
-        countIterator(nodeIterator);
-        assertThat(visited.count()).isEqualTo(treeSize);
+        Iterator<TreeEntry<Optional<Node>>> nodeIterator = Trees.bfsWalk(nodeTree).iterator();
+        while(nodeIterator.hasNext()) { nodeIterator.next();}
+        assertThat(visited[0]).isEqualTo(treeSize);
 
         Optional<Tree<Node>> optionalTree = Trees.optional(nodeTree);
 //        Iterable<Node> iterable = Trees.bfsWalk(optionalTree.get());
@@ -128,59 +126,30 @@ public class TreeMapTest
 //        assertThat(optionalTree.isPresent()).isFalse();
     }
 
-    class Node
-    {
+    class Node {
         Edge edge;
         Vertex vertex;
 
-        Node(Vertex vertex, Edge edge)
-        {
+        Node(Vertex vertex, Edge edge) {
             this.vertex = vertex;
             this.edge = edge;
         }
 
-        public Edge edge()
-        {
+        public Edge edge() {
             return edge;
         }
 
-        public Vertex vertex()
-        {
+        public Vertex vertex() {
             return vertex;
         }
     }
 
-    private int countIterator(Iterable<?> iterable)
-    {
-        int count = 0;
-        for (Object o : iterable)
-        {
-            count++;
-        }
-        return count;
-    }
-
     // add the edge to the subgraph, the other side of the edge will be made symbolic
-    protected void addEdgeToSubgraph(MutableSubgraph sg, ID sg_id, EdgeID edge_id)
-    {
+    protected void addEdgeToSubgraph(MutableSubgraph sg, ID sg_id, EdgeID edge_id) {
         final Direction d = GraphUtilities.directionOppositeTo(edge_id, sg_id);
         final ID other = GraphUtilities.getSymbolicID(GraphUtilities.getOppositeId(edge_id, sg_id));
         final Subgraph.Direction dd = d.opposite().equals(Direction.IN) ? Subgraph.Direction.INWARDS : Subgraph.Direction.OUTWARDS;
         sg.beginEdge(edge_id.label(), other, dd);
     }
 
-    class Counter
-    {
-        int count = 0;
-
-        public void increment()
-        {
-            count++;
-        }
-
-        public int count()
-        {
-            return count;
-        }
     }
-}
